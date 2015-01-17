e43dc0ac69dd486b4547ae90372d481fb3cdcf60
Riecher <riecherd@yahoo.com>
2015-01-17 17:11:50 -0600

    Update RobotTemplate.java
    1/17/15
    Tested this program to run on the roborio:
    Did not run correctly

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,66 +1,31 @@
-package edu.wpi.first.wpilibj.templates;
+package org.usfirst.frc.team4553.robot;
+
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.SimpleRobot;
-import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Gyro;
 
-public class RobotTemplate extends SimpleRobot {
-    RobotDrive chassis = new RobotDrive(0, 1);
+public class Robot extends SampleRobot {
+	RobotDrive chassis = new RobotDrive(0, 1);
     Joystick mainStick = new Joystick(1);
-    Victor compressor = new Victor(2);
-    Victor valve = new Victor(3);
-    DoubleSolenoid tank = new DoubleSolenoid(1, 8);
-    
-    public void autonomous() {
+    Gyro gyro = new Gyro(1);
+
+ 
+   public void autonomous() {
+	   
+   }
         
-    }
+
+ 
     public void operatorControl() {
         chassis.setSafetyEnabled(true);
-        
-        while (isOperatorControl() && isEnabled()) { //Main teleop loop
-            chassis.arcadeDrive(mainStick.getY(), -1 * mainStick.getX(), true); //squared inputs, x value inverted
-            Timer.delay(0.1); //Must be 0.01 because of setSafetyEnabled
+        while (isOperatorControl() && isEnabled()) {
+            chassis.mecanumDrive_Cartesian(mainStick.getY(), -1 * mainStick.getX(), mainStick.getThrottle(), gyro.getAngle());
             
-            if(mainStick.getRawButton(2)){  //Activate compressor
-                compressor.set(1.0);
-            }
-            
-            if(mainStick.getRawButton(3)){ //Deactivate compressor
-                compressor.set(0.0);
-            }
-            
-            if(mainStick.getRawButton(1)){ //Button 3 Hold to open trigger valve
-                valve.set(1.0);
-            }else{
-                valve.set(0.0);}
-            
-            
+            Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
         }
     }
-    public void test() {
-        chassis.setSafetyEnabled(true);
-        while (isTest() && isEnabled()) { //test loop
-            if(mainStick.getRawButton(2)){  //Activate compressor
-                    compressor.set(1.0);
-            }
-        
-            if(mainStick.getRawButton(3)){ //Deactivate compressor
-                compressor.set(0.0);
-            }
-            
-            if(mainStick.getRawButton(1)){ //Button 3 activated open valve (sustained open)
-                valve.set(1.0);
-            }else{
-                valve.set(0.0);}
-        
-            if(mainStick.getRawButton(6)){ //Button 6 activates double solenoid valve
-                tank.set(DoubleSolenoid.Value.kForward);
-            }else{
-                tank.set(DoubleSolenoid.Value.kOff);}
-        }
-        
-    }
+    
 }
